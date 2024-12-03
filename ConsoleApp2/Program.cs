af37b4d9d23d24653a8588b5396e7cc51937637a
NemirovskyMax <artemjaik@gmail.com>
2024-12-03 10:40:29 +0300

    task1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {//Поменять местами содержимое двух строковых переменных
+        static void Swap(ref string x, ref string y)
+        {
+            string p = x;
+            x = y;
+            y = p;
+        }
+        static void Main(string[] args)
+        {
+            string st1 = "это первая строка";
+            string st2 = "это вторая строка";
+            Console.WriteLine($"вывод до обмена:{st1}\t{st2}");
+            Swap(ref st1, ref st2);
+            Console.WriteLine($"вывод после обмена:{st1}\t{st2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    
+    internal class Program
+    {
+        /// <summary>
+        /// величина а меняется значением с величиной b, а величина c — с величиной d.
+        /// </summary>
+        /// <param name="a">int</param>
+        /// <param name="b">int</param>
+        static void Swap(ref int a, ref int b)
+        {
+            int p= a;
+            a=b;
+            b=p;
+        }
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Out(int a)
+        {
+
+        Console.WriteLine($"вывод :{a}"); 
+        }
+        static void Main(string[] args)
+        {
+            int a = Input("a =");
+            int b = Input("b =");
+            int c = Input("c =");
+            int d = Input("d =");
+            Swap(ref a, ref b);
+            Swap(ref c, ref d);
+            Out(a);
+            Out(b);
+            Out(c);
+            Out(d);
+            Console.ReadKey();
+        }
+    }
+}
